3bd39eabda92ce460c4ed29879c8b8a46e18166b
heming-zhang <hemingzhang@wustl.edu>
2020-07-17 11:30:40 -0500

    change number

--- parse_file.py
+++ parse_file.py
@@ -320,5 +320,5 @@ if __name__ == "__main__":
     # DOING K-FOLD VALIDATION IN 100% DATASET
     random_mode = False
     k = 5
-    place_num = 5
+    place_num = 1
     k_fold_split(random_mode, k, place_num)
